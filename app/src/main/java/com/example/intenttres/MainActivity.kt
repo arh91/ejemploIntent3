ebb0028d197bd6f490d2ddd5125b4c6f4f5a2e70
arh91 <alvaro_rh_991@hotmail.com>
2023-02-06 01:07:55 +0100

    Trabajando con código para añadir los fragments a los contenedores del activity_main(land).

--- app/src/main/java/com/example/intenttres/MainActivity.kt
+++ app/src/main/java/com/example/intenttres/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.intenttres
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.intenttres.main.OneFragment
+import com.example.intenttres.main.TwoFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,17 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.fragmentContainerOne, OneFragment.newInstance())
                 .commitNow()
         }*/
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val one = OneFragment()
+        fragmentTransaction.add(R.id.fragmentContainerOne, one)
+        fragmentTransaction.commit()
+
+        val two = TwoFragment()
+        fragmentTransaction.add(R.id.fragmentContainerTwo, two)
+        fragmentTransaction.commit()
 
 
         val siguiente = findViewById<Button>(R.id.btnSiguiente)
